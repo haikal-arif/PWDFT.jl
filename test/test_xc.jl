c576bf386e92c143b2dd5625b9062d5c6ec34722
Muhammad Haikal A <haikal20001@outlook.com>
2023-03-25 08:35:10 +0700

    Add unit test for SCAN exchange functional

--- test/test_xc.jl
+++ test/test_xc.jl
@@ -1,4 +1,7 @@
 rho = [0.1, 0.2, 0.3, 0.4, 0.5]
+sigma = [0.2, 0.3, 0.4, 0.5, 0.6]
+lapl = [0.2, 0.3, 0.4, 0.5, 0.6]
+
 @testset "LDA_VWN xc" begin
     @test calc_epsxc_VWN(LibxcXCCalculator(), rho) ≈ [-0.396206, -0.490557, -0.556226, -0.608272, -0.652089] atol = 1e-5
     @test calc_Vxc_VWN(LibxcXCCalculator(), rho) ≈ [-0.51789, -0.64225, -0.728923, -0.797671, -0.85558] atol = 1e-5
@@ -8,5 +11,7 @@ end
 #     @test calc_epsxc_PBE(rho, sigma) ≈ [-0.459808, -0.5073, -0.562455, -0.611123, -0.653534] atol=1e-5
 # end
 @testset "MGGA_SCAN xc" begin
-    @test calc_Ex_SCAN(LibxcXCCalculator(), rho) ≈ [-0.51789, -0.64225, -0.728923, -0.797671, -0.85558] atol = 1e-5
+    # From Libxc
+    @test XC_x_scan(rho, norm∇ρ, τ) ≈ [-0.408368, -0.471540, -0.533396, -0.591370, -0.642808] atol = 1e-5
 end
+
