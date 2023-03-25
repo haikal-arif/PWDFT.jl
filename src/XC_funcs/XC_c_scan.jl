bdf248cc9acce39bd3e53be1984d86e3717ddfd3
Muhammad Haikal A <haikal20001@outlook.com>
2023-03-25 08:49:16 +0700

    Added TODO implementation of SCAN correlation

---
+++ src/XC_funcs/XC_c_scan.jl
@@ -0,0 +1,10 @@
+function XC_c_scan(ρ, norm∇ρ, τ)
+  # TODO! we need to implement this according to paper
+  # If we assume that ρ isn't spin polarized
+  # ζ is 0
+  rₛ = (3 / (4 * π * n))^3
+
+  sc = 1
+
+  return sc, v1c, v2c, v3c
+end
