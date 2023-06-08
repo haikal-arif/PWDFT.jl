f6542979f6d4b6aec5f27fc3c1a5d81e3dc3e7ec
Muhammad Haikal A <haikal20001@outlook.com>
2023-06-08 14:10:27 +0700

    bugfix for PW lsda potential

--- src/XC_funcs/XC_c_lsda.jl
+++ src/XC_funcs/XC_c_lsda.jl
@@ -7,7 +7,7 @@ function XC_c_lsda(n::Float64)
   # We assume there is no spin polarization so we only have εc(rs, 0) term
 
   rs = (3 / (4 * π * n))^(1 / 3)
-  DrsDn = (1 / 3) * (3 / (4 * π))^(1 / 3) * n^(-4 / 3)
+  DrsDn = (1 / 3) * rs / n
 
   A = 0.031091
   alpha = 0.21370
@@ -16,12 +16,13 @@ function XC_c_lsda(n::Float64)
   β3 = 1.6382
   β4 = 0.49294
 
-  denom = 2 * A * (β1 * rs^0.5 + β2 * rs + β3 * rs^1.5 + β4 * rs * rs)
-  DdenomDrs = A * (β1 * rs^-0.5 + 2 * β2 + 3 * β3 * rs^0.5 + 4 * β4 * rs)
-  DdenomDn = DdenomDrs * DrsDn
+  Q0 = -2 * A * (1 + alpha * rs)
+  Q1 = 2 * A * (β1 * rs^0.5 + β2 * rs + β3 * rs^1.5 + β4 * rs * rs)
+  Q1prime = A * (β1 * rs^-0.5 + 2 * β2 + 3 * β3 * rs^0.5 + 4 * β4 * rs)
 
-  ec = -2 * A * (1 + alpha * rs) * log(1 + 1 / denom)
-  vc = -2 * A * ((1 + alpha * DrsDn) * log(1 + 1 / denom) - (1 + alpha * rs) / (denom * denom + denom) * DdenomDn)
+  ec = Q0 * log(1 + 1 / Q1)
+  vc = (-2 * A * alpha * log(1 + 1 / Q1) - (Q0 * Q1prime) / (Q1 * Q1 + Q1)) * DrsDn
+
 
   return ec, vc
 end
